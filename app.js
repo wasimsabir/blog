0a64fd9ff64819ba84e9683d7e7093905c412c6f
wasim.ali393@gmail.com <wasim.ali393@gmail.com>
2018-06-01 15:35:16 +0530

    implemented blog routes in separate files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var wasimExpress = require("express"),
   var localStrategy = require("passport-local");
   var passportLocalMongoose = require("passport-local-mongoose");
   var Blog = require("./models/blog");
+  var blogRoutes = require("./routes/blog");
 app.set("view engine", "ejs");
 app.use(wasimExpress.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
@@ -29,6 +30,8 @@ passport.deserializeUser(User.deserializeUser());
 app.use(expressSanitizer()); // must use after bodyParser use, and before routes
 app.use(methodOverride('_method'));
 
+app.use(blogRoutes);
+
 //database connection blogy
 mongoose.connect("mongodb://localhost/blogApp");
 
@@ -43,100 +46,7 @@ db.once('open', function () {
     res.redirect("/blogs");
   });
 
-  //Restful Index route
-  app.get("/blogs", function (req, res) {
-    Blog.find({}, function (err, foundBlog) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("blog/index", {
-          blog: foundBlog
-        });
-      }
-
-    }); // index route end
-
-  });
-
-  // Restful New route
-  app.get("/blogs/new", function (req, res) {
-    res.render("blog/new");
-  }); // new route end
-
-  // Restful create route
-  app.post("/blogs", function (req, res) {
-    console.log(req.body.post.description);
-    req.body.post.description = req.sanitize(req.body.post.description);
-    console.log("=============");
-    console.log(req.body.post.description);
-    Blog.create(req.body.post, function (err, createdPost) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(createdPost);
-        res.redirect("blogs");
-      }
-
-    }); //blog-create end
-
-  }); // Create route end
-
-  // Restful show route
-  app.get("/blogs/:id",isLoggedIn, function (req, res) {
-    Blog.findById(req.params.id, function (err, foundPost) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("blog/show", {
-          "post": foundPost
-        });
-      }
-
-    }); // blog find ends
-
-  }); //End show route
-
-
-  //Restful edit route
-  app.get("/blogs/:id/edit", function (req, res) {
-    Blog.findById(req.params.id, function (err, postFound) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("blog/edit", {
-          post: postFound
-        });
-      }
-    }); // end blog find
-  }); // end edit route
-
-  //Restful update route
-  app.put("/blogs/:id", function (req, res) {
-    req.body.post.description = req.sanitize(req.body.post.description);
-    Blog.findByIdAndUpdate(req.params.id, req.body.post, function (err, postUpdated) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.redirect("/blogs/" + req.params.id);
-      }
-    }); // end find by id and update
-  }); // end update route
-
-  // Restful delete route
-  app.delete("/blogs/:id", function(req, res){
-    
-    // find by id and remove
-    Blog.findByIdAndRemove(req.params.id, function(err){
-      if(err) {
-        res.redirect("/blogs");
-      }
-      else {
-        res.redirect("/blogs");
-      }
-    }); // end find by id and remove
-  }); // end delete route
-
-
+  
 
 
   // Register route GET
---
+++ routes/blog.js
@@ -0,0 +1,126 @@
+var Blog = require("../models/blog");
+var wasimExpress = require("express");
+var router = wasimExpress.Router();
+
+//Restful Index route
+router.get("/blogs", function (req, res) {
+  Blog.find({}, function (err, foundBlog) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("blog/index", {
+        blog: foundBlog
+      });
+    }
+
+  });  // Blog.find ends
+
+
+}); // index route end
+
+
+
+
+// Restful New route
+router.get("/blogs/new", function (req, res) {
+  res.render("blog/new");
+}); // new route end
+
+
+
+
+// Restful create route
+router.post("/blogs", function (req, res) {
+  req.body.post.description = req.sanitize(req.body.post.description);
+
+  Blog.create(req.body.post, function (err, createdPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/blogs");
+    }
+
+  }); //blog-create end
+
+}); // Create route end
+
+
+
+// Restful show route
+router.get("/blogs/:id", isLoggedIn, function (req, res) {
+  Blog.findById(req.params.id, function (err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("blog/show", {
+        "post": foundPost
+      });
+    }
+
+  }); // blog find ends
+
+}); //End show route
+
+
+
+
+//Restful edit route
+router.get("/blogs/:id/edit", function (req, res) {
+  Blog.findById(req.params.id, function (err, postFound) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("blog/edit", {
+        post: postFound
+      });
+    }
+  }); // end blog find
+}); // end edit route
+
+
+
+
+//Restful update route
+router.put("/blogs/:id", function (req, res) {
+  req.body.post.description = req.sanitize(req.body.post.description);
+  Blog.findByIdAndUpdate(req.params.id, req.body.post, function (err, postUpdated) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  }); // end find by id and update
+}); // end update route
+
+
+
+
+// Restful delete route
+router.delete("/blogs/:id", function(req, res){
+  
+  // find by id and remove
+  Blog.findByIdAndRemove(req.params.id, function(err){
+    if(err) {
+      res.redirect("/blogs");
+    }
+    else {
+      res.redirect("/blogs");
+    }
+  }); // end find by id and remove
+}); // end delete route
+
+
+//middleware
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
+
+module.exports = router;
+
+
+
+
